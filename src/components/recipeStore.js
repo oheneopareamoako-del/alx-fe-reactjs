c75c9ee7bbb90d7c1dc2e97e36c0b4ce184bc447
Lawrencia Ohene Opare Amoako <oheneopareamoako@gmail.com>
2025-12-03 12:24:29 +0000

    Fix recipeStore.js: ensure updateRecipe and deleteRecipe actions

--- src/components/recipeStore.js
+++ src/components/recipeStore.js
@@ -2,22 +2,13 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
-
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(r => r.id === updatedRecipe.id ? updatedRecipe : r)
+  })),
+  deleteRecipe: (id) => set(state => ({
+    recipes: state.recipes.filter(r => r.id !== id)
+  })),
   setRecipes: (recipes) => set({ recipes }),
 }));
 
