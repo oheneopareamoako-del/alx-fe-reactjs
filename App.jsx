384bd870151d6d832471106ae9f311dd9cd5ae6c
Lawrencia Ohene Opare Amoako <oheneopareamoako@gmail.com>
2025-12-03 12:01:01 +0000

    Added recipe details, edit, and delete functionality

--- App.jsx
+++ App.jsx
@@ -2,19 +2,36 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
-import RecipeDetail from './components/RecipeDetail';
-import EditRecipeForm from './components/EditRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<><AddRecipeForm /><RecipeList /></>} />
-        {/* Additional routes for RecipeDetail or EditRecipeForm */}
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route
+          path="/recipes/:id"
+          element={<RecipeDetailsWrapper />}
+        />
       </Routes>
     </Router>
   );
 }
+
+// Wrapper to extract recipeId from URL
+import { useParams } from 'react-router-dom';
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={Number(id)} />;
+};
 
 export default App;
 
--- DeleteRecipeButton.jsx
+++ DeleteRecipeButton.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useRecipeStore } from './recipeStore';
 
-const DeleteRecipeButton = ({ id }) => {
+const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
-  return <button onClick={() => deleteRecipe(id)}>Delete</button>;
+
+  return (
+    <button onClick={() => deleteRecipe(recipeId)}>
+      Delete Recipe
+    </button>
+  );
 };
 
 export default DeleteRecipeButton;
